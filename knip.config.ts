7c5470c8c23dbdb93067d9c75cd9224ef1e209b1
Ashwin Mahadevan <mahadevan@ashwinm.com>
2026-06-10 08:22:41 -0700

    Format fix

--- knip.config.ts
+++ knip.config.ts
@@ -1,3 +1,8 @@
 export default {
-	ignore: ['src/lib/common/data/EscapeSequences.ts', 'src/lib/common/buffer/Constants.ts']
+	ignore: [
+		'src/lib/common/data/EscapeSequences.ts',
+		'src/lib/common/buffer/Constants.ts',
+		'src/lib/common/input/KittyKeyboard.ts',
+		'src/lib/common/input/Win32InputMode.ts'
+	]
 };
