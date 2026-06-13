07cdb3990151ab7268e1b6c718122ddde4f049e8
Ashwin Mahadevan <mahadevan@ashwinm.com>
2026-06-12 19:24:29 -0700

    Ignore globals, node-gyp, and svelte-check in knip.

--- knip.config.ts
+++ knip.config.ts
@@ -1,3 +1,4 @@
 export default {
-	ignore: ['src/lib/common/data/EscapeSequences.ts', 'src/lib/common/buffer/Constants.ts']
+	ignore: ['src/lib/common/data/EscapeSequences.ts', 'src/lib/common/buffer/Constants.ts'],
+	ignoreDependencies: ['globals', 'node-gyp', 'svelte-check']
 };
