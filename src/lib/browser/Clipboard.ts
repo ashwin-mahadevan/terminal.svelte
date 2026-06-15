9e322757278baa55c9b1f092d4b10fd97b2f8cc5
Ashwin Mahadevan <mahadevan@ashwinm.com>
2026-06-15 11:25:41 -0700

    Inline prepareTextForTerminal and bracketTextForPaste into paste() and delete them.
    Delete Clipboard.svelte.test.ts since it only tested the now-inlined functions.

--- src/lib/browser/Clipboard.ts
+++ src/lib/browser/Clipboard.ts
@@ -6,42 +6,22 @@
 import type { OptionsService } from '$lib/common/services/OptionsService';
 import type { CoreService } from '$lib/common/services/CoreService';
 
-/**
- * Prepares text to be pasted into the terminal by normalizing the line endings
- * @param text The pasted text that needs processing before inserting into the terminal
- */
-export function prepareTextForTerminal(text: string): string {
-	return text.replace(/\r?\n/g, '\r');
-}
-
-/**
- * Bracket text for paste, if necessary, as per https://cirw.in/blog/bracketed-paste
- * @param text The pasted text to bracket
- */
-export function bracketTextForPaste(text: string, bracketedPasteMode: boolean): string {
-	if (!bracketedPasteMode) {
-		return text;
-	}
-	// Sanitize pasted text to prevent injected escape sequences (e.g. exiting bracketed paste)
-	// by replacing ESC (\x1b) with its visible representation U+241B (␛).
-	// TODO: Fix this upstream type error.
-	// eslint-disable-next-line no-control-regex
-	const sanitizedText = text.replace(/\x1b/g, '\u241b');
-	return `\x1b[200~${sanitizedText}\x1b[201~`;
-}
-
 export function paste(
 	text: string,
 	textarea: HTMLTextAreaElement,
 	coreService: CoreService,
 	optionsService: OptionsService
 ): void {
-	text = prepareTextForTerminal(text);
-	text = bracketTextForPaste(
-		text,
+	text = text.replace(/\r?\n/g, '\r');
+	const bracketedPasteMode =
 		coreService.decPrivateModes.bracketedPasteMode &&
-			optionsService.rawOptions.ignoreBracketedPasteMode !== true
-	);
+		optionsService.rawOptions.ignoreBracketedPasteMode !== true;
+	if (bracketedPasteMode) {
+		// Sanitize pasted text to prevent injected escape sequences (e.g. exiting bracketed paste)
+		// by replacing ESC (\x1b) with its visible representation U+241B (␛).
+		// eslint-disable-next-line no-control-regex
+		text = `\x1b[200~${text.replace(/\x1b/g, '␛')}\x1b[201~`;
+	}
 	coreService.triggerDataEvent(text, true);
 	textarea.value = '';
 }
