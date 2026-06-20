7c36a1d3cc4e93e7286eb1df269dc5abab040a66
Ashwin Mahadevan <mahadevan@ashwinm.com>
2026-06-19 21:54:20 -0700

    cleaner

--- src/lib/stream-parser.ts
+++ src/lib/stream-parser.ts
@@ -1,5 +1,6 @@
 interface Cell {
-	content: string;
+	text: string;
+	width: number;
 }
 
 interface Buffer {
@@ -25,36 +26,55 @@ interface Events {
 	bell(): void;
 }
 
+const decoder = new TextDecoder();
+const segmenter = new Intl.Segmenter()
 
-function parser(
+export function parser(
 	state: State,
-	events: Events
+	events: Readonly<Events>
 ) {
-	function OSC(...args: unknown[]): number {
-		// handle Operating System Commands; return the number of bytes consumed.
+	function write(chunk: Uint8Array) {
+		let index = 0;
+		let start;
+
+		while ((start = index) < chunk.length) {
+			// Does this check work for non-ascii? We should accept any utf-8 eventually.
+			if ((chunk[index] >= 32) && (chunk[index] < 127)) {
+				// printable, so just advance the index.
+				index += 1;
+				continue;
+			}
+			// we have a control sequence, so flush the printables.
+
+			const str = decoder.decode(chunk.subarray(start, index));
+
+			for (const { segment } of segmenter.segment(str)) {
+
+				const cell = {
+					text: segment,
+					width: 1,
+				} satisfies Cell
+
+				// we'd do the actual state update here.
+				console.log(cell.width, cell.text);
+			}
+
+			// now handle the specific control sequence
+
+			if (chunk[index] === 0x7b) {
+				// handle the actual control sequence.
+				events.bell();
+
+				// advance by the length of control sequence; csi, etc would be larger.
+				index += 1;
+				continue;
+			}
+
+			// when we reach here we should have handled all control sequence cases,
+			// and we'd already handled the printables.
+			throw new Error()
+		}
 	}
 
-	// csi, etc.
-
-	let parsermode = "ground";
-	// declare other parse-internal state here.
-
-	return new WritableStream<Uint8Array>({
-		write(chunk) {
-			// handle chunk of codepoints, updating state and dispatching events
-			let bytesConsumed = 0;
-
-			while (bytesConsumed < chunk.length) {
-				const byte = chunk[bytesConsumed]
-
-				if (byte === 0x07) {
-					events.bell();
-					bytesConsumed += 1;
-					continue;
-				}
-
-				// handle other escape sequences, print, etc.
-			}
-		}
-	})
+	return new WritableStream<Uint8Array>({ write })
 }
