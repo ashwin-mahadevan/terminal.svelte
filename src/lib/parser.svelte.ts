e1b692081d3f786e82e59a0b5292a2d9096a10e0
Ashwin Mahadevan <mahadevan@ashwinm.com>
2026-06-26 23:24:00 -0700

    switch

--- src/lib/parser.svelte.ts
+++ src/lib/parser.svelte.ts
@@ -5,15 +5,6 @@ export type Events = {
 	bell?: () => void;
 };
 
-const decoder = new TextDecoder();
-const segmenter = new Intl.Segmenter();
-
-function* decode_segments(chunk: Uint8Array) {
-	for (const { segment } of segmenter.segment(decoder.decode(chunk))) {
-		yield segment;
-	}
-}
-
 export class Emulator {
 	state = new State();
 
@@ -31,41 +22,49 @@ export class Emulator {
 
 	write = (chunk: Uint8Array) => {
 		let index = 0;
-		let start;
 
-		while ((start = index) < chunk.length) {
-			// Collect printable characters.
-			while (index < chunk.length) {
-				if (chunk[index] < 0x20) break;
-				if (chunk[index] === 0x7f) break;
+		while (index < chunk.length) {
+			const byte = chunk[index];
+			index += 1;
 
-				index += 1;
-			}
+			switch (byte) {
+				// NUL (null)
+				case 0x00:
+					console.log('NUL');
+					break;
 
-			// Flush printable characters.
-			for (const segment of decode_segments(chunk.subarray(start, index))) {
-				// autowrap: if x is past the last column, wrap before writing.
-				if (this.state.column >= this.state.columns) {
-					this.state.buffer[this.state.row].break = true;
-					this.state.column = 0;
-					this.lineFeed();
-				}
+				// SOH (start of heading)
+				case 0x01:
+					console.log('SOH');
+					break;
 
-				this.state.buffer[this.state.row].cells[this.state.column] = {
-					text: segment,
-					attrs: this.state.attributes
-				} satisfies Cell;
+				// STX (start of text)
+				case 0x02:
+					console.log('STX');
+					break;
 
-				this.state.column += 1;
-			}
+				// ETX (end of text)
+				case 0x03:
+					console.log('ETX');
+					break;
 
-			if (index === chunk.length) break;
+				// EOT (end of transmission)
+				case 0x04:
+					console.log('EOT');
+					break;
 
-			// now check which control sequence we're handling
-			switch (chunk[index]) {
+				// ENQ (enquiry)
+				case 0x05:
+					console.log('ENQ');
+					break;
+
+				// ACK (acknowledge)
+				case 0x06:
+					console.log('ACK');
+					break;
+
 				// BEL (bell)
 				case 0x07:
-					index += 1;
 					this.events.bell?.();
 					break;
 
@@ -74,31 +73,144 @@ export class Emulator {
 					if (this.state.column > 0) {
 						this.state.column -= 1;
 					}
-					index += 1;
 					break;
 
-				// CR (carriage return)
-				case 0x0d:
-					index += 1;
-					this.state.column = 0;
+				// HT (horizontal tab)
+				case 0x09:
+					console.log('HT');
 					break;
 
 				// LF (line feed)
 				case 0x0a:
-					index += 1;
 					this.lineFeed();
+					break;
+
+				// VT (vertical tab)
+				case 0x0b:
+					console.log('VT');
+					break;
+
+				// FF (form feed)
+				case 0x0c:
+					console.log('FF');
+					break;
+
+				// CR (carriage return)
+				case 0x0d:
+					this.state.column = 0;
+					break;
+
+				// SO (shift out)
+				case 0x0e:
+					console.log('SO');
+					break;
+
+				// SI (shift in)
+				case 0x0f:
+					console.log('SI');
+					break;
+
+				// DLE (data link escape)
+				case 0x10:
+					console.log('DLE');
+					break;
+
+				// DC1 (device control 1, XON)
+				case 0x11:
+					console.log('DC1');
+					break;
+
+				// DC2 (device control 2)
+				case 0x12:
+					console.log('DC2');
+					break;
+
+				// DC3 (device control 3, XOFF)
+				case 0x13:
+					console.log('DC3');
+					break;
+
+				// DC4 (device control 4)
+				case 0x14:
+					console.log('DC4');
+					break;
+
+				// NAK (negative acknowledge)
+				case 0x15:
+					console.log('NAK');
+					break;
+
+				// SYN (synchronous idle)
+				case 0x16:
+					console.log('SYN');
+					break;
+
+				// ETB (end of transmission block)
+				case 0x17:
+					console.log('ETB');
+					break;
+
+				// CAN (cancel)
+				case 0x18:
+					console.log('CAN');
+					break;
+
+				// EM (end of medium)
+				case 0x19:
+					console.log('EM');
+					break;
+
+				// SUB (substitute)
+				case 0x1a:
+					console.log('SUB');
 					break;
 
 				// ESC (escape)
 				case 0x1b:
 					console.log('ESC');
-					index += 1;
 					break;
 
-				default:
-					// Unknown control byte: consume and ignore so we never crash on a
-					// sequence we don't model. The MWE only needs the cases above.
-					index += 1;
+				// FS (file separator)
+				case 0x1c:
+					console.log('FS');
+					break;
+
+				// GS (group separator)
+				case 0x1d:
+					console.log('GS');
+					break;
+
+				// RS (record separator)
+				case 0x1e:
+					console.log('RS');
+					break;
+
+				// US (unit separator)
+				case 0x1f:
+					console.log('US');
+					break;
+
+				// DEL (delete)
+				case 0x7f:
+					console.log('DEL');
+					break;
+
+				// Printable Character
+				default: {
+					// autowrap: if x is past the last column, wrap before writing.
+					if (this.state.column >= this.state.columns) {
+						this.state.buffer[this.state.row].break = true;
+						this.state.column = 0;
+						this.lineFeed();
+					}
+
+					this.state.buffer[this.state.row].cells[this.state.column] = {
+						text: String.fromCharCode(byte), // this only works for ascii.
+						attrs: this.state.attributes
+					} satisfies Cell;
+
+					this.state.column += 1;
+				}
 			}
 		}
 	};
