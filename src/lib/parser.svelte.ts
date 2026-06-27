1d926862030f055db494c468c86d1fbcbdae0f3f
Ashwin Mahadevan <mahadevan@ashwinm.com>
2026-06-26 23:56:00 -0700

    Order the control character cases by byte value.
    All C0 controls (0x00-0x1f) plus DEL were already handled but scattered; arranging them in ascending order makes the coverage easy to verify.

--- src/lib/parser.svelte.ts
+++ src/lib/parser.svelte.ts
@@ -28,29 +28,21 @@ export class Emulator {
 			index += 1;
 
 			switch (byte) {
-				// Intentionally ignored.
+				// NUL, SOH, STX, ETX, EOT (intentionally ignored)
 				case 0x00: // NUL (null)
 				case 0x01: // SOH (start of heading)
 				case 0x02: // STX (start of text)
 				case 0x03: // ETX (end of text)
 				case 0x04: // EOT (end of transmission)
-				case 0x06: // ACK (acknowledge)
-				case 0x10: // DLE (data link escape)
-				case 0x12: // DC2 (device control 2)
-				case 0x14: // DC4 (device control 4)
-				case 0x15: // NAK (negative acknowledge)
-				case 0x16: // SYN (synchronous idle)
-				case 0x17: // ETB (end of transmission block)
-				case 0x19: // EM (end of medium)
-				case 0x1c: // FS (file separator)
-				case 0x1d: // GS (group separator)
-				case 0x1e: // RS (record separator)
-				case 0x1f: // US (unit separator)
 					break;
 
 				// ENQ (enquiry)
 				case 0x05:
 					throw new Error('NOT IMPLEMENTED');
+
+				// ACK (acknowledge)
+				case 0x06:
+					break;
 
 				// BEL (bell)
 				case 0x07:
@@ -80,12 +72,50 @@ export class Emulator {
 
 				case 0x0e: // SO (shift out)
 				case 0x0f: // SI (shift in)
-				case 0x11: // DC1 (device control 1, XON)
-				case 0x13: // DC3 (device control 3, XOFF)
-				case 0x18: // CAN (cancel)
+					throw new Error('NOT IMPLEMENTED');
+
+				// DLE (data link escape)
+				case 0x10:
+					break;
+
+				// DC1 (device control 1, XON)
+				case 0x11:
+					throw new Error('NOT IMPLEMENTED');
+
+				// DC2 (device control 2)
+				case 0x12:
+					break;
+
+				// DC3 (device control 3, XOFF)
+				case 0x13:
+					throw new Error('NOT IMPLEMENTED');
+
+				case 0x14: // DC4 (device control 4)
+				case 0x15: // NAK (negative acknowledge)
+				case 0x16: // SYN (synchronous idle)
+				case 0x17: // ETB (end of transmission block)
+					break;
+
+				// CAN (cancel)
+				case 0x18:
+					throw new Error('NOT IMPLEMENTED');
+
+				// EM (end of medium)
+				case 0x19:
+					break;
+
 				case 0x1a: // SUB (substitute)
 				case 0x1b: // ESC (escape)
-				case 0x7f: // DEL (delete)
+					throw new Error('NOT IMPLEMENTED');
+
+				case 0x1c: // FS (file separator)
+				case 0x1d: // GS (group separator)
+				case 0x1e: // RS (record separator)
+				case 0x1f: // US (unit separator)
+					break;
+
+				// DEL (delete)
+				case 0x7f:
 					throw new Error('NOT IMPLEMENTED');
 
 				// Printable Character
